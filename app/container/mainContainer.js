967d94bb2e05de104182d352e0c229b1dacb5783
iwenchao <iwenchaos@gmail.com>
2018-05-24 13:30:27 +0800

    增加基础样式颜色资源

--- app/container/mainContainer.js
+++ app/container/mainContainer.js
@@ -1,9 +1,8 @@
-
-
 import React from 'react';
 import {
     StyleSheet,
 } from 'react-native';
+import ScrollableTabView from "react-native-scrollable-tab-view";
 
 
 const styles = StyleSheet.create(
@@ -15,18 +14,35 @@ const styles = StyleSheet.create(
 );
 
 const TAB_BAR_RESO = [
-    [require('../images/home.png'),require('../images/home_active.png')],
+    [require('../images/home.png'), require('../images/home_active.png')],
+    [require('../images/reading.png'), require('../images/reading_active.png')],
+    [require('../images/music.png'), require('../images/music_active.png')],
+    [require('../images/movie.png'), require('../images/movie_active.png')]
 
 ];
 
-class MainContainer extends React.Component{
+class MainContainer extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
+    render() {
+        return (<ScrollableTabView
+            tabBarPosition="bottom"
+            locked={true}
+            scrollWithoutAnimation={true}
+            prerenderingSiblingsNumber={4}
+            renderTabBar={()=>{
+                return <TabBar >
+            }
+            }
+        >
+
+        </ScrollableTabView>)
+    }
 
 
 }
 
-export  default MainContainer
+export default MainContainer
---
+++ app/styles/commonStyle.js
@@ -0,0 +1,14 @@
+const colors = {
+    MAIN_COLOR: 'white',
+    GRAY_COLOR: '#DADADA',
+    TEXT_GRAY_COLOR: '#79767C',
+    TEXT_COLOR: '#443046',
+    BACKGROUND_COLOR: '#F7F7F7',
+    LIGHT_BLUE_COLOR: '#5CACEE',//浅蓝色
+    LIGHT_GRAY_COLOR: '#cccccc44'//浅灰色
+}
+
+export default {
+    ...colors,
+    HORIZONTAL_SPACE: 10,
+}
